b32a748716e440e5381240f734bc0bbf62223aa7
mark5041 <mark.beloso5041@gmail.com>
2022-02-02 15:26:30 +0100

    header and footer

---
+++ components/header.php
@@ -0,0 +1,5 @@
+<header>
+    <nav class="navbar">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+    </nav>
+</header>
